8d5c2bf2ae3dba4fa3557883225c82ae9f21aa83
William Hernandez Tellez <williamhernandeztellez@gmail.com>
2022-09-25 20:07:34 -0500

    P1

---
+++ P17S2120222HTW/app/src/main/java/com/example/p17s2120222htw/MainActivity.java
@@ -0,0 +1,14 @@
+package com.example.p17s2120222htw;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
